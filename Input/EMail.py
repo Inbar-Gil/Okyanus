3f0f5f46fce8be9e065e27c47712b4aab9ab6959
Inbar Gil <inbar.gil.41@gmail.com>
2021-04-19 17:02:16 +0300

    Fixed Git Tree

--- Input/EMail.py
+++ Input/EMail.py
@@ -1,3 +1,15 @@
-"""
-Contains the functions for analyzing email addresses
-"""
+import re
+# boolean function from RE tavnit
+# function: XXX@mail.huji.ac.il to [XXX,mail,huji.ac.il]
+if __name__ == "__main__":
+    paragraph = input("Enter paragraph you want to extract emails from: ")
+
+    emails = re.findall(r'[a-zA-Z0-9._-]+@[\w.-]+\.com', paragraph)
+
+    print(emails)
+
+    with open("C:/Users/t8747382/Desktop/Okyanus/Okyanus/Tests/tests.txt", 'r') as f:
+        queries = f.readlines()[:-1]
+        for q in queries:
+            print(re.findall(r'[a-zA-Z0-9._-]+@[\w.-]+\.com', q))
+
