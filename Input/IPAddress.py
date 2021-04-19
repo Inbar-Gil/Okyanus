313de33748b7318cce2bd1c6cd182529ce62af3d
Gal <gr.gal2004@gmail.com>
2021-04-19 20:13:47 +0300

    First version
    1.0

--- Input/IPAddress.py
+++ Input/IPAddress.py
@@ -1,3 +1,16 @@
-"""
-Contains the functions for analyzing IP addresses
-"""
+import re
+
+#string that will check
+regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+
+#function to return regex
+def check(Ip):
+    if (re.search(regex, Ip)):
+        print("Valid Ip address")
+
+    else :
+        print("Invalid Ip address")
+
+
+ip_input = input("Enter ip addres you want to check: ")
+check(ip_input)
