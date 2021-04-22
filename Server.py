0af08bdd3637c4afc9896d49ac5feab6f9ec472b
Inbar Gil <inbar.gil.41@gmail.com>
2021-04-22 23:40:45 +0300

    Initialize Django

--- Server.py
+++ Server.py
@@ -1,6 +1,8 @@
 """
 This File contains the main code running the script and Server interface
 """
+import django
 
 if __name__ == "__main__":
-    pass
+    django
+    print(django.get_version())
