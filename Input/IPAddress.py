c6d834201eb8d6d64cf35952ce6fa965f8f3a6e2
Gal <gr.gal2004@gmail.com>
2021-04-22 23:07:48 +0300

    Ipv4 and Ipv6
    add a function that will check what internet protocol it is

--- Input/IPAddress.py
+++ Input/IPAddress.py
@@ -1,24 +1,58 @@
 import re
 
-# string that will check
-regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+VALID_IP = False
+# regex string for validating an Ip-address
+REGEX_IP = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+# regex string for validating an its a ipv6
+RE_IPV6="(\A([0-9a-f]{1,4}:){1,1}(:[0-9a-f]{1,4}){1,6}\Z)|(\A([0-9a-f]{1,4}:){1,2}(:[0-9a-f]{1,4}){1,5}\Z)|(\A([0-9a-f]{1,4}:){1,3}(:[0-9a-f]{1,4}){1,4}\Z)|(\A([0-9a-f]{1,4}:){1,4}(:[0-9a-f]{1,4}){1,3}\Z)|(\A([0-9a-f]{1,4}:){1,5}(:[0-9a-f]{1,4}){1,2}\Z)|(\A([0-9a-f]{1,4}:){1,6}(:[0-9a-f]{1,4}){1,1}\Z)|(\A(([0-9a-f]{1,4}:){1,7}|:):\Z)|(\A:(:[0-9a-f]{1,4}){1,7}\Z)|(\A((([0-9a-f]{1,4}:){6})(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3})\Z)|(\A(([0-9a-f]{1,4}:){5}[0-9a-f]{1,4}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3})\Z)|(\A([0-9a-f]{1,4}:){5}:[0-9a-f]{1,4}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,1}(:[0-9a-f]{1,4}){1,4}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,2}(:[0-9a-f]{1,4}){1,3}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,3}(:[0-9a-f]{1,4}){1,2}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,4}(:[0-9a-f]{1,4}){1,1}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A(([0-9a-f]{1,4}:){1,5}|:):(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A:(:[0-9a-f]{1,4}){1,5}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)"
 
-
-# todo change var name in global standard (all capital letters) to REGEX_IP
-# todo add explanation comment for RegEx or make it more understandable
 
 # function to return regex
-def check(Ip):  # todo change function name to fit API (isQueryIPAddress)
-    if (re.search(regex, Ip)):
-        print("Valid Ip address")  # todo instead of prints make return values true or false
+def isQueryIPAddress(Ip):
+
+    if re.search(REGEX_IP, Ip) :
+        return VALID_IP == True
+        isInternetProtocol(Ip)
+
 
     else:
-        print("Invalid Ip address")
-
-# todo add function for analyzing IPv4 or IPv6
+        return VALID_IP == False
 
 
-if __name__ == "__main__":
-    # todo add more comprehensive tests to check special cases for IP's
-    ip_input = input("Enter ip addres you want to check: ")
-    check(ip_input)
+#function for analyzing IPv6
+def isInternetProtocol(Ip):
+
+    count_ipv4 = 0
+    count_ipv6 = 0
+    bool_ipv6 = False
+    bool_ipv4 = True
+    if re.search(RE_IPV6,Ip):
+        bool_ipv6 = True
+    else:
+        bool_ipv4 = True
+
+
+def isInternetProtocol2(Ip):
+    count_ipv4 = 0
+    count_ipv6 = 0
+    for i in range(0,len(Ip),1):
+        if "." in Ip :
+            count_ipv4 += 1
+        elif ":" in Ip:
+            count_ipv6 += 1
+    if count_ipv4 == 3:
+        return "IPv4"
+    elif count_ipv4 >= 4:
+        return "IPv6"
+    elif count_ipv6 > 0:
+        return "IPv6"
+
+if __name__ == '__main__' :
+
+    Ip = "366.1.2.2"
+    print(isQueryIPAddress(Ip))
+    Ip = "64ba:c4c9:60ee:1f20:e034:b8ab:49d2:ee2e"
+    print(isQueryIPAddress(Ip))
+    Ip = "6db8:1058:b70:63e0:f52c:3c9e:ac11:ab64"
+    print(isQueryIPAddress(Ip))
+
