5c9ab318f7357a867a0b8985afb03cd4d6168ebc
Yuval Navon <yuvalnavon8@gmail.com>
2021-04-22 19:21:49 +0300

    merge check

--- Input/EMailTests.py
+++ Input/EMailTests.py
@@ -3,71 +3,44 @@ import re
 
 # todo change all functions to work on one single email, checking lists is not necessary
 
-def Email_Check_Bool(paragraph):  # todo change function name to fit API (isQueryEmail)
-    emails = re.findall(r'([a-zA-Z0-9._-]+@[\w.-]+)', paragraph)
-    if len(emails) > 0:
-        for q in emails:
-            if q.count("@") == 1:
-                Shtrudel = q.find("@")  # todo change shtrudel varname to atSignIndex
-                prefix = q[0:Shtrudel]
+def fit_API(isQueryEmail):  # todo change function name to fit API (isQueryEmail)
+    email = re.findall(r'([a-zA-Z0-9._-]+@[\w.-]+)', isQueryEmail)
+    email = "".join(email)
+    return email
+
+
+def prefix_Check(preFormatEmail):
+    if preFormatEmail.count("@") == 1:
+        count = 0
+        atSignIndex = preFormatEmail.find("@")  # todo change shtrudel varname to atSignIndex
+        prefix = preFormatEmail[0:atSignIndex]
+        for char in prefix:  # todo make it so when this rule is broken raise error instead of returning false
+            # todo this for loop doesn't work for example test abc..def@gmail.com
+            # todo I want this for loop to be in a seperate function prefixCheck for later use
+            if char == "." or char == "_" or char == "-":
+                count += 1
+            else:
                 count = 0
-                for char in prefix:  # todo make it so when this rule is broken raise error instead of returning false
-                    # todo this for loop doesn't work for example test abc..def@gmail.com
-                    # todo I want this for loop to be in a seperate function prefixCheck for later use
-                    if char == "." or char == "_" or char == "-":
-                        count += 1
-                    else:
-                        count = 0
-                    if count == 2:
-                        break
-                    return True
-        return False
-    return False
+            if count == 2:
+                raise ValueError("This email is not legal")
+        return True
+    raise ValueError("This email is not legal")
 
 
-def Email_Check_List(paragraph):  # todo this function isn't needed since we check one email at at time
-    valid_email_list = []
-    emails = re.findall(r'([a-zA-Z0-9._-]+@[\w.-]+)', paragraph)
-    print(emails)
-    if len(emails) > 0:
-        for q in emails:
-            valid = True
-            if q.count("@") == 1:
-                Shtrudel = q.find("@")
-                prefix = q[0:Shtrudel]
-                count = 0
-                for char in prefix:
-                    if char == "." or char == "_" or char == "-":
-                        count += 1
-                    else:
-                        count = 0
-                    if count == 2:
-                        valid = False
-            if valid:
-                valid_email_list.append(q)
-    return valid_email_list
-
-
-def Email_Piruk(paragraph):  # todo change function name to fit API (formatEmail)
-    lister = []
-    email_list = re.findall(r'([a-zA-Z0-9._-]+@[\w.-]+)', paragraph)
-    for email in Email_Check_List(paragraph):
-        Shtrudel = email.find("@")
-        period = email.find(".")
-        prefix = email[0:Shtrudel]
-        domain = email[Shtrudel + 1:period]  # todo fix bug that happens for period in the prefix
-        ending = email[period:]
-        lister1 = [prefix, domain, ending]
-        lister.append(lister1)
-    for x in lister:
-        print(x)  # todo no printing in functions, only in tests, return the email piruk
+def format_Email(formatEmail):
+    atSignIndex = formatEmail.find("@")
+    afterAtSignIndex = formatEmail[atSignIndex:]
+    periodSignIndex = afterAtSignIndex.find(".")
+    prefix = formatEmail[0:atSignIndex]
+    domain = afterAtSignIndex[1:periodSignIndex]
+    ending = afterAtSignIndex[periodSignIndex:]
+    lister1 = [prefix, domain, ending]
+    return lister1
 
 
 if __name__ == "__main__":
     # todo write more comprehensive tests, checking all the cases
     text = input("Enter paragraph:")
-    print(Email_Check_Bool(text))
-    print("")
-    print(Email_Check_List(text))
-    print("")
-    Email_Piruk(text)
+    if (prefix_Check(fit_API(text))):
+        formattedEmail = fit_API(text)
+        print(format_Email(formattedEmail))
