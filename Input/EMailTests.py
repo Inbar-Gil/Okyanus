c113c1bc59c62d8423f74a0156b10282904ac681
Inbar Gil <inbar.gil.41@gmail.com>
2021-04-22 19:34:19 +0300

    Updated todos, fit api
    make isQueryEmail a boolean function and then this feature is done

--- Input/EMailTests.py
+++ Input/EMailTests.py
@@ -1,10 +1,8 @@
 import re
 
 
-# todo change all functions to work on one single email, checking lists is not necessary
-
-def fit_API(isQueryEmail):  # todo change function name to fit API (isQueryEmail)
-    email = re.findall(r'([a-zA-Z0-9._-]+@[\w.-]+)', isQueryEmail)
+def isQueryEmail(query):  # todo this should be a boolean function
+    email = re.findall(r'([a-zA-Z0-9._-]+@[\w.-]+)', query)
     email = "".join(email)
     return email
 
@@ -12,11 +10,9 @@ def fit_API(isQueryEmail):  # todo change function name to fit API (isQueryEmail
 def prefix_Check(preFormatEmail):
     if preFormatEmail.count("@") == 1:
         count = 0
-        atSignIndex = preFormatEmail.find("@")  # todo change shtrudel varname to atSignIndex
+        atSignIndex = preFormatEmail.find("@")
         prefix = preFormatEmail[0:atSignIndex]
-        for char in prefix:  # todo make it so when this rule is broken raise error instead of returning false
-            # todo this for loop doesn't work for example test abc..def@gmail.com
-            # todo I want this for loop to be in a seperate function prefixCheck for later use
+        for char in prefix:
             if char == "." or char == "_" or char == "-":
                 count += 1
             else:
@@ -27,11 +23,11 @@ def prefix_Check(preFormatEmail):
     raise ValueError("This email is not legal")
 
 
-def format_Email(formatEmail):
-    atSignIndex = formatEmail.find("@")
-    afterAtSignIndex = formatEmail[atSignIndex:]
+def formatEmail(query):
+    atSignIndex = query.find("@")
+    afterAtSignIndex = query[atSignIndex:]
     periodSignIndex = afterAtSignIndex.find(".")
-    prefix = formatEmail[0:atSignIndex]
+    prefix = query[0:atSignIndex]
     domain = afterAtSignIndex[1:periodSignIndex]
     ending = afterAtSignIndex[periodSignIndex:]
     lister1 = [prefix, domain, ending]
@@ -39,8 +35,7 @@ def format_Email(formatEmail):
 
 
 if __name__ == "__main__":
-    # todo write more comprehensive tests, checking all the cases
-    text = input("Enter paragraph:")
-    if (prefix_Check(fit_API(text))):
-        formattedEmail = fit_API(text)
-        print(format_Email(formattedEmail))
+    query = input("Enter paragraph:")
+    if prefix_Check(isQueryEmail(query)):
+        formattedEmail = isQueryEmail(query)
+        print(formatEmail(formattedEmail))
