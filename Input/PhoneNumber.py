49d1973365e77ed8594282ded740592482346974
Roni Elbaz <elbazroni65@gmail.com>
2021-04-24 13:17:32 +0300

    Two Functions

--- Input/PhoneNumber.py
+++ Input/PhoneNumber.py
@@ -4,18 +4,32 @@ Contains the functions for analyzing phone numbers
 
 import re
 
+PHONE_NUMBER_TYPE = {1 : "Intl", 2 : "Mobl", 3 : "Home"}
 
 def isQueryPhoneNumber(query):
     #Remove white characters
     pattern = re.compile(r'\s+')
     query = re.sub(pattern, '', query)
 
-    phoneIntl = re.match(r'\+\d{3}-?\d{9}', query, re.IGNORECASE)
+    phoneIntl = re.match(r'\+\d{3}-?[0-9]{7}([0-9]{2})?([0-9])?', query, re.IGNORECASE)
     phoneMobileOrHome = re.match(r'\d{3}-?\d{3}-?\d{4}', query, re.IGNORECASE)
+    oldPhoneHomeNumber = re.match(r'0[23489]-?\d{3}-?\d{4}', query, re.IGNORECASE)
 
-    return isMatchPhoneNumber(query, phoneMobileOrHome) or isMatchPhoneNumber(query, phoneIntl)
+    return isMatchPhoneNumber(query, phoneMobileOrHome) or isMatchPhoneNumber(query, phoneIntl) or isMatchPhoneNumber(query, oldPhoneHomeNumber)
 
 
 def isMatchPhoneNumber(query, finalSerachPhoneNumber):
     return finalSerachPhoneNumber != None and finalSerachPhoneNumber.group(0) == query
+
+
+def findPhoneNumberType(phoneNumber):
+    # *** Get phone number assuming it is OK ***
+
+    digitsHomePhoneNumber = [2, 3, 4, 7, 8, 9]
+
+    if phoneNumber[0] == "+":
+        return PHONE_NUMBER_TYPE[1]
+    if int(phoneNumber[1]) in digitsHomePhoneNumber:
+        return PHONE_NUMBER_TYPE[3]
+    return PHONE_NUMBER_TYPE[2]
 
