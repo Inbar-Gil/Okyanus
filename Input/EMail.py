4aeebdaeb8f7b9555e22512fb47e3c1b104182f3
Yuval Navon <yuvalnavon8@gmail.com>
2021-04-28 18:05:52 +0300

    Email function name fixes and back to original file
    changed the functions names to fit the API file, made the first function a boolean function and moved the code back to Email.py

--- Input/EMail.py
+++ Input/EMail.py
@@ -1,16 +1,36 @@
 import re
-# boolean function from RE tavnit
-# function: XXX@mail.huji.ac.il to [XXX,mail,huji.ac.il]
+
+
+def isQueryEmail(query):  # todo this should be a boolean function
+    if query.count("@") == 1:
+        count = 0
+        atSignIndex = query.find("@")
+        prefix = query[0:atSignIndex]
+        for char in prefix:
+            if char == "." or char == "_" or char == "-":
+                count += 1
+            else:
+                count = 0
+            if count == 2:
+                raise ValueError("This email is not legal")
+        return True
+    raise ValueError("This email is not legal")
+
+
+def formatEmail(query):
+    atSignIndex = query.find("@")
+    afterAtSignIndex = query[atSignIndex:]
+    periodSignIndex = afterAtSignIndex.find(".")
+    prefix = query[0:atSignIndex]
+    domain = afterAtSignIndex[1:periodSignIndex]
+    ending = afterAtSignIndex[periodSignIndex:]
+    lister1 = [prefix, domain, ending]
+    return lister1
+
+
 if __name__ == "__main__":
-    paragraph = input("Enter paragraph you want to extract emails from: ")
-
-    emails = re.findall(r'[a-zA-Z0-9._-]+@[\w.-]+\.com', paragraph)
-
-    print(emails)
-    print (len(emails))
-
-    with open("C:/Users/t8747382/Desktop/Okyanus/Okyanus/Tests/tests.txt", 'r') as f:
-        queries = f.readlines()[:-1]
-        for q in queries:
-            print(re.findall(r'[a-zA-Z0-9._-]+@[\w.-]+\.com', q))
-
+    query = input("Enter paragraph:")
+    email = re.findall(r'([a-zA-Z0-9._-]+@[\w.-]+)', query)
+    email1 = "".join(email)
+    if (isQueryEmail(email1) == True):
+        print (formatEmail(email1))
