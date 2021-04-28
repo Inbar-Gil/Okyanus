3623bd1dc89fef4376d9bbe80b6538920e0ad4ea
Inbar Gil <inbar.gil.41@gmail.com>
2021-04-28 17:29:44 +0300

    IP Address Final

--- Input/IPAddress.py
+++ Input/IPAddress.py
@@ -4,55 +4,32 @@ VALID_IP = False
 # regex string for validating an Ip-address
 REGEX_IP = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 # regex string for validating an its a ipv6
-RE_IPV6="(\A([0-9a-f]{1,4}:){1,1}(:[0-9a-f]{1,4}){1,6}\Z)|(\A([0-9a-f]{1,4}:){1,2}(:[0-9a-f]{1,4}){1,5}\Z)|(\A([0-9a-f]{1,4}:){1,3}(:[0-9a-f]{1,4}){1,4}\Z)|(\A([0-9a-f]{1,4}:){1,4}(:[0-9a-f]{1,4}){1,3}\Z)|(\A([0-9a-f]{1,4}:){1,5}(:[0-9a-f]{1,4}){1,2}\Z)|(\A([0-9a-f]{1,4}:){1,6}(:[0-9a-f]{1,4}){1,1}\Z)|(\A(([0-9a-f]{1,4}:){1,7}|:):\Z)|(\A:(:[0-9a-f]{1,4}){1,7}\Z)|(\A((([0-9a-f]{1,4}:){6})(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3})\Z)|(\A(([0-9a-f]{1,4}:){5}[0-9a-f]{1,4}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3})\Z)|(\A([0-9a-f]{1,4}:){5}:[0-9a-f]{1,4}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,1}(:[0-9a-f]{1,4}){1,4}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,2}(:[0-9a-f]{1,4}){1,3}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,3}(:[0-9a-f]{1,4}){1,2}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A([0-9a-f]{1,4}:){1,4}(:[0-9a-f]{1,4}){1,1}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A(([0-9a-f]{1,4}:){1,5}|:):(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)|(\A:(:[0-9a-f]{1,4}){1,5}:(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\Z)"
+RE_IPV6 = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
 
 
 # function to return regex
 def isQueryIPAddress(Ip):
-
-    if re.search(REGEX_IP, Ip) :
-        return VALID_IP == True
-        isInternetProtocol(Ip)
-
-
+    if re.search(REGEX_IP, Ip) or re.search(RE_IPV6, Ip):
+        return True
     else:
-        return VALID_IP == False
+        return False
 
 
-#function for analyzing IPv6
-def isInternetProtocol(Ip):
-
-    count_ipv4 = 0
-    count_ipv6 = 0
-    bool_ipv6 = False
-    bool_ipv4 = True
-    if re.search(RE_IPV6,Ip):
-        bool_ipv6 = True
-    else:
-        bool_ipv4 = True
-
-
-def isInternetProtocol2(Ip):
-    count_ipv4 = 0
-    count_ipv6 = 0
-    for i in range(0,len(Ip),1):
-        if "." in Ip :
-            count_ipv4 += 1
+def formatIPAddress(Ip):
+    for i in range(0, len(Ip), 1):
+        if "." in Ip:
+            return "IPv4", Ip
         elif ":" in Ip:
-            count_ipv6 += 1
-    if count_ipv4 == 3:
-        return "IPv4"
-    elif count_ipv4 >= 4:
-        return "IPv6"
-    elif count_ipv6 > 0:
-        return "IPv6"
+            return "IPv6", Ip
 
-if __name__ == '__main__' :
 
-    Ip = "366.1.2.2"
+if __name__ == '__main__':
+    Ip = "254.1.2.2"
     print(isQueryIPAddress(Ip))
+    print(formatIPAddress(Ip))
     Ip = "64ba:c4c9:60ee:1f20:e034:b8ab:49d2:ee2e"
     print(isQueryIPAddress(Ip))
-    Ip = "6db8:1058:b70:63e0:f52c:3c9e:ac11:ab64"
+    print(formatIPAddress(Ip))
+    Ip = "6db8:1058:b70:63e0:f52c:39e:ac11:ab64"
     print(isQueryIPAddress(Ip))
-
+    print(formatIPAddress(Ip))
