5c50956229bb84adb8a55fbe766fc49fb4d05f67
Inbar Gil <inbar.gil.41@gmail.com>
2021-05-03 17:47:24 +0300

    RE Analyzer Start

--- Input/RegExAnalyzer.py
+++ Input/RegExAnalyzer.py
@@ -1,10 +1,13 @@
 """
 contains the code which analyzes the query and chooses the sites to search
 """
-
+from Input.EMail import EMail
+from Input.IPAddress import IP
+from Input.EMail import Phone
 
 class RegExAnalyzer:
     def __init__(self, query):
+        self.query = query
         self.queryType = ""
         self.data = []
 
@@ -14,6 +17,15 @@ class RegExAnalyzer:
         sets query type and data accordingly
         :return:
         """
+        email = EMail(self.query).getQueryData()
+        if email[0]:
+            self.queryType = "EMAIL"
+            self.data = email[1]
 
     def returnData(self):
         return self.queryType, self.data
+
+
+if __name__ == "__main__":
+    with open("tests.txt", 'r') as f:
+        lines = f.readlines()
