535c9bc9adb8e4583e57b5a5ca84de1282fce47b
Gal <gr.gal2004@gmail.com>
2021-05-09 00:07:47 +0300

    Dictionary to return everything we want.
    Added a json import that will change the json to dict and added a sorting system that will add everything we need into one dictionary.

--- Output/IPAddress.py
+++ Output/IPAddress.py
@@ -2,11 +2,12 @@
 This file contains functions for searching IP websites
 """
 import urllib.request
+import json
 
 
 def searchIPInfo(IP):
     request_url = urllib.request.urlopen('http://ipinfo.io/'+IP+'?token=c2b88a2d6f552a')
-    print(request_url.read())
+    return request_url.read()
 
 
 
@@ -14,17 +15,35 @@ def searchIPInfo(IP):
 def searchIPAPI(IP):
 
     request_url = urllib.request.urlopen('http://ip-api.com/json/'+ IP)
-    print(request_url.read())
+    return request_url.read()
 
 
 def getIPResponse(IP):
-    """
-    #"isp","org","as"
-    #hostname - loc
-    :param IP:
-    :return: all data
-    """
+    output_dic = {}
+    data_file = searchIPAPI(IP)
+    data_file2 = searchIPInfo(IP)
+    Ip_dict = json.loads(data_file)
+    Ip_dict2 = json.loads(data_file2)
+    for i in Ip_dict2.keys():
+        if i == "hostname":
+            output_dic[i] = Ip_dict2[i]
+        if i == "city":
+            output_dic[i] = Ip_dict2[i]
+        if i == "region":
+            output_dic[i] = Ip_dict2[i]
+        if i == "loc":
+            output_dic[i] = Ip_dict2[i]
+
+    for i in Ip_dict.keys():
+        if i == "isp":
+            output_dic[i] = Ip_dict[i]
+        if i == "org":
+            output_dic[i] = Ip_dict[i]
+        if i == "as":
+            output_dic[i] = Ip_dict[i]
+
+    print(output_dic)
 
 
-searchIPInfo("221.119.149.238")
-searchIPAPI("221.119.149.238")
+
+getIPResponse("221.119.149.238")
