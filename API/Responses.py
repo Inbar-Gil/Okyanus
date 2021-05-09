dc4b39673e0b0639b116e7ce9111b9fd88fffa26
Yuval Navon <yuvalnavon8@gmail.com>
2021-05-09 22:13:51 +0300

    NoErrorResponse class
    made the class lol

--- API/Responses.py
+++ API/Responses.py
@@ -1,9 +1,31 @@
 from API.API import Response
 import re
 
+EMAIL_SITES = []
+IP_SITES = []
+PHONE_SITES = []
+
 
 class ErrorResponse(Response):
     def __init__(self, query, error):
         super().__init__(query)
         self.exception = re.split("'", str(error.__class__))[1]
         self.message = error.__str__()
+
+
+class NoResponse(Response):
+    def __init__(self, query, queryType):
+        super().__init__(query)
+        self.queryType = queryType
+        self.siteList = []
+        self.setSites()
+
+    def setSites(self):
+        if self.queryType == "EMAIL":
+            self.siteList = EMAIL_SITES
+
+        elif self.queryType == "IP":
+            self.siteList = IP_SITES
+
+        elif self.queryType == "PHONE":
+            self.siteList = PHONE_SITES
