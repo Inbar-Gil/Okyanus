3659f1b8195ce18425ca8e533f02673d0b02fd13
Roni Elbaz <elbazroni65@gmail.com>
2021-05-12 14:34:40 +0300

    Phone Function

--- Output/PhoneNumber.py
+++ Output/PhoneNumber.py
@@ -1,3 +1,20 @@
 """
 This file contains functions for searching phone number websites
 """
+import requests as req
+
+
+def phoneOwnerInformation(infoPhoneNumber):
+    resp = req.get("https://441il.com/en/looktra.php?area=" + infoPhoneNumber[1][0] + "&phone=" + infoPhoneNumber[2])
+    infoHtmlList = resp.text.split("<TD>")[1:]
+    place = 0
+    for i in infoHtmlList:
+        indexSymbol = infoHtmlList[place].index("<")
+        infoHtmlList[place] = infoHtmlList[place][:indexSymbol]
+        place += 1
+
+    #Returns: Name, Adress
+    return [infoHtmlList[3], infoHtmlList[4]]
+
+
+print(phoneOwnerInformation(["ed", ("08",), "6272343"]))
