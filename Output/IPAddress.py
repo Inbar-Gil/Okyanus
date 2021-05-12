c3f10bc88b4f0cf9ca100f48fa2964d3568174c1
Yuval Navon <yuvalnavon8@gmail.com>
2021-05-12 14:15:50 +0300

    IP Address Output

--- Output/IPAddress.py
+++ Output/IPAddress.py
@@ -3,47 +3,59 @@ This file contains functions for searching IP websites
 """
 import urllib.request
 import json
+from API.API import Site
 
 
-def searchIPInfo(IP):
-    request_url = urllib.request.urlopen('http://ipinfo.io/'+IP+'?token=c2b88a2d6f552a')
-    return request_url.read()
+class Ipinfo(Site):
+    def __init__(self, query):
+        super().__init__(query)
+
+    def searchSite(self):
+        output_dic = {}
+        data_file = self.searchIPInfo()
+        Info_dict = json.loads(data_file)
+        for i in Info_dict.keys():
+            if i == "hostname":
+                output_dic[i] = Info_dict[i]
+            if i == "city":
+                output_dic[i] = Info_dict[i]
+            if i == "region":
+                output_dic[i] = Info_dict[i]
+            if i == "loc":
+                output_dic[i] = Info_dict[i]
+        return output_dic
+
+    def searchIPInfo(self):
+        request_url = urllib.request.urlopen('http://ipinfo.io/' + self.data + '?token=c2b88a2d6f552a')
+        return request_url.read()
 
 
+class IpApi(Site):
+    def __init__(self, query):
+        super().__init__(query)
+
+    def searchIPAPI(self):
+        request_url = urllib.request.urlopen('http://ip-api.com/json/' + self.data)
+        return request_url.read()
+
+    def searchSite(self):
+        output_dic = {}
+        data_file = self.searchIPAPI()
+        Api_Dict = json.loads(data_file)
+        for i in Api_Dict.keys():
+            if i == "isp":
+                output_dic[i] = Api_Dict[i]
+            if i == "org":
+                output_dic[i] = Api_Dict[i]
+            if i == "as":
+                output_dic[i] = Api_Dict[i]
+        return output_dic
 
 
-def searchIPAPI(IP):
+test = IpApi("5.109.46.92")
+result = test.searchSite()
+print(result)
 
-    request_url = urllib.request.urlopen('http://ip-api.com/json/'+ IP)
-    return request_url.read()
-
-
-def getIPResponse(IP):
-    output_dic = {}
-    data_file = searchIPAPI(IP)
-    data_file2 = searchIPInfo(IP)
-    Ip_dict = json.loads(data_file)
-    Ip_dict2 = json.loads(data_file2)
-    for i in Ip_dict2.keys():
-        if i == "hostname":
-            output_dic[i] = Ip_dict2[i]
-        if i == "city":
-            output_dic[i] = Ip_dict2[i]
-        if i == "region":
-            output_dic[i] = Ip_dict2[i]
-        if i == "loc":
-            output_dic[i] = Ip_dict2[i]
-
-    for i in Ip_dict.keys():
-        if i == "isp":
-            output_dic[i] = Ip_dict[i]
-        if i == "org":
-            output_dic[i] = Ip_dict[i]
-        if i == "as":
-            output_dic[i] = Ip_dict[i]
-
-    print(output_dic)
-
-
-
-getIPResponse("221.119.149.238")
+test = Ipinfo("5.109.46.92")
+result = test.searchSite()
+print(result)
