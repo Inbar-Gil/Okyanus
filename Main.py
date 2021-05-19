d5e293ecf6f4ebe47c86834274fb7e8ffd187023
Yuval Navon <yuvalnavon8@gmail.com>
2021-05-19 22:54:25 +0300

    Relative import for Main

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 """
 This File contains the main code running the script and Server interface
 """
-from Input.RegExAnalyzer import *
-from Output.SiteSearch import *
-from API.Responses import *
-import okyanus.manage
+from .Input.RegExAnalyzer import *
+from .Output.SiteSearch import *
+from .API.Responses import *
+from .okyanus import manage
 import sys
 
 
